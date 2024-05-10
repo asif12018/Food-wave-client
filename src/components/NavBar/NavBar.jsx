a44d2aac25fb2e151890cd628141126845208ac6
asif12018 <asif.request@gmail.com>
2024-05-10 15:07:06 +0600

    banner section added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -20,7 +20,7 @@ const NavBar = () => {
   
   return (
 
-    <header className="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full py-7">
+    <header className="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full py-7 max-w-[1400px] mx-auto">
       <nav className="relative max-w-7xl w-full flex flex-wrap md:grid md:grid-cols-12 basis-full items-center px-4 md:px-6 md:px-8 mx-auto" aria-label="Global">
         <div className="md:col-span-3 flex items-center">
 
