8baab6b379542f77ce597757b9ec80ebd608239a
asif12018 <asif.request@gmail.com>
2024-05-11 11:18:36 +0600

    search suggestion feature added

--- src/Pages/AllFood/AllFood.jsx
+++ src/Pages/AllFood/AllFood.jsx
@@ -1,15 +1,77 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Cards from "../../components/Cards/Cards";
+import { ReactSearchAutocomplete } from 'react-search-autocomplete';
+
 
 const AllFood = () => {
     const [foods, setFoods] = useState([])
+    const [items, setItems] = useState([])
     useEffect(()=>{
        axios.get('http://localhost:5000/allFood')
-       .then(data => setFoods(data.data))
+       .then(data => {
+        // setItems(data.data)
+        setFoods(data.data)
+        //creating a new array 
+        const newArray = data.data.map(obj =>{
+            return {
+                id: obj._id,
+                name: obj.itemName
+            }
+        })
+
+        setItems(newArray)
+    
+    
+    }
+    
+    
+    )
     },[])
+    // console.log(items)
+
+    //react search auto suggestion intrigation
+    const handleOnSearch = (string, results) => {
+        // onSearch will have as the first callback parameter
+        // the string searched and for the second the results.
+        console.log(string, results)
+      }
+
+      const handleOnHover = (result) => {
+        // the item hovered
+        // console.log(result)
+      }
+
+      const handleOnSelect = (item) => {
+        // the item selected
+        console.log(item)
+      }
+
+      const handleOnFocus = () => {
+        // console.log('Focused')
+      }
+
+      const formatResult = (item) => {
+        return (
+          <>
+            <span style={{ display: 'block', textAlign: 'left' }}>id: {item.id}</span>
+            <span style={{ display: 'block', textAlign: 'left' }}>name: {item.name}</span>
+          </>
+        )
+      }
     return (
         <div className="max-w-[1400px] mx-auto">
+            <div className="" style={{ width: "50%" }}>
+          <ReactSearchAutocomplete
+            items={items}
+            onSearch={handleOnSearch}
+            onHover={handleOnHover}
+            onSelect={handleOnSelect}
+            onFocus={handleOnFocus}
+            autoFocus
+            formatResult={formatResult}
+          />
+        </div>
             <h3 className="text-2xl font-bold">Total Available Food:{foods.length}</h3>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                  {
--- src/components/FeatureFood/FeatureFood.jsx
+++ src/components/FeatureFood/FeatureFood.jsx
@@ -4,12 +4,17 @@ import Cards from "../Cards/Cards";
 import { Link } from "react-router-dom";
 
 const FeatureFood = () => {
-    const [foods, setFoods] = useState([])
-    const [featuredFood, setFeaturedFood] = useState([])
+    const [foods, setFoods] = useState([]);
+   
+    const [featuredFood, setFeaturedFood] = useState([]);
+    
+    
+
     useEffect(()=>{
         axios.get('http://localhost:5000/allFood')
         .then(data => {
             setFoods(data.data);
+            
             //filtering food with highest quantity
             const maxFood = Math.max(...data.data.map(food => food.quantity));
             const filteredFeaturedFood = data.data.filter(food => food.quantity == maxFood);
@@ -19,14 +24,18 @@ const FeatureFood = () => {
 
     return (
         <div className="my-10">
+            
+            
+
             <h3 className="text-4xl font-bold text-center">Feature Foods</h3>
+            
             <div className="grid grid-cols-1 md:grid-cols-3 mx-auto gap-10 my-10">
                 {
                     featuredFood.map(food => <Cards key={food._id} food={food}></Cards>)
                 }
             </div>
             <div className="flex justify-center">
-                <Link className="btn">Show All Food</Link>
+                <Link to={'/allFood'} className="btn">Show All Food</Link>
             </div>
         </div>
     );
